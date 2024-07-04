8b20dbaadd5866af0e02dbc8cecde06e8fb0b1d5
Vanessa Iniguez <vanessainiguez98@gmail.com>
2024-07-03 23:32:47 -0500

    [IMPROVE] Improve User Model

--- app/users/domain/models/user.py
+++ app/users/domain/models/user.py
@@ -1,18 +1,10 @@
-class User:
-    def __init__(self, identifier: str, name: str, lastname: str, age: int, email: str):
-        self.identifier = identifier
-        self.name = name
-        self.lastname = lastname
-        self.age = age
-        self.email = email
+from dataclasses import dataclass
 
-    def __eq__(self, other: "User") -> bool:
-        if not isinstance(other, User):
-            return False
-        return (
-            self.identifier == other.identifier and
-            self.name == other.name and
-            self.lastname == other.lastname and
-            self.age == other.age and
-            self.email == other.email
-        )
+
+@dataclass
+class User:
+    identifier: str
+    name: str
+    lastname: str
+    age: int
+    email: str
